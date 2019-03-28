e77ff3f96c9f352cd0273f22d3cd654107944d4d
logangarza <garzal@uwplatt.edu>
2019-03-27 23:30:41 -0500

    Depth first search. Added barebones

---
+++ DfsSolver.py
@@ -0,0 +1,20 @@
+def moveLeft(self, posX, posY):
+    posX[0] = posX[0] + 1
+    posY[0] = posY[0] + 0
+
+
+def moveRight(self, posX, posY):
+    posX[0] = posX[0] - 1
+    posY[0] = posY[0] + 0
+
+
+def moveUp(self, posX, posY):
+    posX[0] = posX[0] + 0
+    posY[0] = posY[0] + 1
+
+
+def moveDown(self, posX, posY):
+    posX[0] = posX[0] + 0
+    posY[0] = posY[0] - 1
+
+
