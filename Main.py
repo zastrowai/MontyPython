e5ec122527b7b414e82fc9664f4e449c2934b992
Aidan <zastrowai@uwplatt.edu>
2019-03-27 23:24:34 -0500

    Maze Moves and Checks

--- Main.py
+++ Main.py
@@ -1,4 +1,10 @@
 from bin.Maze import Maze
 
 test = Maze()
-print(test.width)
+left = [1]
+right = [1]
+
+
+print(test.checkOpen(int(left[0]), int(right[0])))
+print(test.checkWall(left[0], right[0]))
+print(test.checkExit(left[0], right[0]))
--- bin/Maze.py
+++ bin/Maze.py
@@ -20,5 +20,27 @@ class Maze:
                 [1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 3],
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
-    def test(self):
-        print("testing")
+    def moveLeft(self, posX, posY):
+        posX[0] = posX[0] + 1
+        posY[0] = posY[0] + 0
+
+    def moveRight(self, posX, posY):
+        posX[0] = posX[0] - 1
+        posY[0] = posY[0] + 0
+
+    def moveUp(self, posX, posY):
+        posX[0] = posX[0] + 0
+        posY[0] = posY[0] + 1
+
+    def moveDown(self, posX, posY):
+        posX[0] = posX[0] + 0
+        posY[0] = posY[0] - 1
+
+    def checkOpen(self, posX, posY):
+        return self.maze[posX][posY] == 0
+
+    def checkWall(self, posX, posY):
+        return self.maze[posX][posY] == 1
+
+    def checkExit(self, posX, posY):
+        return self.maze[posX][posY] == 3
