06713c185dcb4911302a307d3042fa5e5fdd182e
Aidan <zastrowai@uwplatt.edu>
2019-03-28 16:03:19 -0500

    Maze Print

--- Main.py
+++ Main.py
@@ -5,6 +5,4 @@ left = [1]
 right = [1]
 
 
-print(test.checkOpen(int(left[0]), int(right[0])))
-print(test.checkWall(left[0], right[0]))
-print(test.checkExit(left[0], right[0]))
+test.printMaze()
--- bin/Maze.py
+++ bin/Maze.py
@@ -4,8 +4,8 @@ class Maze:
     ## 2 is start
     ## 3 is goal
     def __init__(self):
-        self.width = 10
-        self.height = 10
+        self.width = 12
+        self.height = 12
         self.maze = [
                 [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -21,20 +21,20 @@ class Maze:
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
     def moveLeft(self, posX, posY):
-        posX[0] = posX[0] + 1
+        posX[0] = posX[0] - 1
         posY[0] = posY[0] + 0
 
     def moveRight(self, posX, posY):
-        posX[0] = posX[0] - 1
+        posX[0] = posX[0] + 1
         posY[0] = posY[0] + 0
 
     def moveUp(self, posX, posY):
         posX[0] = posX[0] + 0
-        posY[0] = posY[0] + 1
+        posY[0] = posY[0] - 1
 
     def moveDown(self, posX, posY):
         posX[0] = posX[0] + 0
-        posY[0] = posY[0] - 1
+        posY[0] = posY[0] + 1
 
     def checkOpen(self, posX, posY):
         return self.maze[posX][posY] == 0
@@ -44,3 +44,13 @@ class Maze:
 
     def checkExit(self, posX, posY):
         return self.maze[posX][posY] == 3
+
+    def printMaze(self):
+        for i in range(len(self.maze)):
+            for j in range(len(self.maze[i])):
+                print(self.maze[i][j], end='')
+            print()
+
+    def visited(self, posX, posY):
+        self.maze[posX][posY] = 'X'
+
