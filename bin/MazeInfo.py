5b8fe5e9dab8daa9cc61bc3a57aae02462561577
Aidan <zastrowai@uwplatt.edu>
2019-04-02 00:03:15 -0500

    commented MazeInfo

--- bin/MazeInfo.py
+++ bin/MazeInfo.py
@@ -3,6 +3,9 @@
 # Storage for Heuristic
 class MazeInfo:
 
+    # Constructor for maze info
+    # takes in a X coordinate, Y Coordinate, The cost to the start, and the Cost to the end
+    # And Calculates f by adding h and g
     def __init__(self, posX, posY, costToStart, costToEnd):
         self.x = posX
         self.y = posY
